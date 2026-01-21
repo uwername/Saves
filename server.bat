1ed98f1b6ee49f0f9f5ad14f1d8747f2455acd0d
uwername <drhallen2@gmail.com>
2026-01-21 23:18:58 +0530

    commit

--- server.bat
+++
@@ -1,4 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-' The 0 at the end runs the command completely hidden
-WshShell.Run "C:\system32\pythonw.exe C:\system32\upload.pyw", 0
-Set WshShell = Nothing
